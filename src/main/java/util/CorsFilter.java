285283f3262d4512d3871c0f8d51b642c975b8b1
JJJHB <1373455726@qq.com>
2026-05-18 21:35:27 +0800

    提交所有修改和新增文件

---
+++ src/main/java/util/CorsFilter.java
@@ -0,0 +1,41 @@
+package util;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/userQueryList") // 或 "/api/*" 如果后端接口统一前缀
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        // 设置跨域头
+        resp.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        resp.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+        resp.setHeader("Access-Control-Max-Age", "3600"); // 缓存预检1小时
+
+        // 如果是预检请求，直接返回 200，不继续传给 Servlet
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        // 放行请求到 Servlet
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+}
--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -1,68 +1,63 @@
 package util;
-//工具包一般用于放一些通用工具类
+
 import java.sql.Connection;
-//数据库连接对象
 import java.sql.DriverManager;
-//用来获取连接
 import java.sql.ResultSet;
-//查询结果集（select返回）
 import java.sql.SQLException;
-//数据库异常
 import java.sql.Statement;
-//执行SQL语句
 
 public class DBUtil {
 
-	static String user="root";
-	static String password="123456";
-	static String url="jdbc:mysql://localhost:3306/springbootohu22anl?useUnicode=true&characterEncoding=utf8";
-	
-	
-//	建立数据库连接并返回一个 Connection 对象。
-	public static Connection getConnection(){
-		Connection conn=null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-//			加载 MySQL 驱动旧版本驱动（MySQL 5）新版（MySQL 8）
-//			是 加载 MySQL JDBC 驱动 的经典写法，它告诉 JVM 去加载 MySQL 的驱动类，
-//			以便之后能通过 DriverManager.getConnection(...) 建立数据库连接。
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			 conn=DriverManager.getConnection(url, user, password);
-//			 获取数据库连接，如果这里失败：这一句不会执行成功
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
+    static String user = "root";
+    static String password = "123456";
+    // MySQL5 兼容 URL：关闭 SSL + UTF-8 编码
+    static String url = "jdbc:mysql://localhost:3306/springbootohu22anl?useUnicode=true&characterEncoding=utf8&useSSL=false";
 
-	public static void closeJDBC(ResultSet rs,Statement stmt,Connection conn){
-//		释放数据库游标资源
-		if(rs!=null){
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-//		Statement 是执行 SQL 的对象 不关会占用资源
-		if(stmt!=null){
-			try {
-				stmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-//		真正断开数据库
-		if(conn!=null){
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    // 建立数据库连接并返回 Connection 对象
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            // MySQL 5 驱动类
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("数据库驱动类未找到，请检查 jar 包是否存在！");
+            e.printStackTrace();
+        }
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("数据库连接成功: " + conn);
+        } catch (SQLException e) {
+            System.err.println("获取数据库连接失败，请检查 URL/用户名/密码/数据库状态！");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    // 释放资源
+    public static void closeJDBC(ResultSet rs, Statement stmt, Connection conn) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
